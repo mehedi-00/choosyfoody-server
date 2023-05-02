b4b81cec9a163dd2e683c8c85e115e137249985d
Mehedi <developermehedi128@gmail.com>
2023-05-02 12:41:51 +0600

    chefs json data added

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 5000;
-
+const chefs = require('./data/chefs.json');
 app.get('/', (req, res) => {
     res.json({ message: 'Hello Server is Running' });
 });
 
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+});
 
 app.listen(port, () => {
     console.log(`Server is runnig port: ${port}`);
