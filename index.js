633560405c122276167270bfbdd9b6df4250987c
Mehedi <developermehedi128@gmail.com>
2023-05-04 11:32:49 +0600

    blog server complete

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(cors());
 const port = process.env.PORT || 5000;
 const chefs = require('./data/chefs.json');
+const blogs = require('./data/blog.json')
 app.get('/', (req, res) => {
     res.json({ message: 'Hello Server is Running' });
 });
@@ -16,6 +17,9 @@ app.get('/chef/:id', (req, res) => {
     const selectedData = chefs.find(n => n.id == id);
     res.send(selectedData);
 });
+app.get('/blogs',(req,res)=>{
+    res.send(blogs)
+})
 
 app.listen(port, () => {
     console.log(`Server is runnig port: ${port}`);
